1e7a37030389f102c9e043f414f2006261a034af
Otepa Veronica <gracaotepastudy@gmail.com>
2024-08-19 20:37:20 +0300

    Second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,12 +2,10 @@
 import java.util.Scanner; // Scanner il permet  de saisir  les donnees qui seront entre
 public class Calculator {  //Le nom de ma classe
     public static void   main( String [] args){
-
         double num1 = 0;  // J'ai inicialize une variable et j'ai lui est declare
         double num2 = 0; //'ai inicialize une variable et j'ai lui est declare
         char operator; // Ici la variable operator n'a pas ete inicialize, elle sera inicialise au moment execution
         double answer = 0.0;  //  C'est la valeur du reponse ou de la sortie
-
         Scanner scanObject = new Scanner (System.in);
         System.out.println("Enter first number :");
         num1 = scanObject.nextDouble();
